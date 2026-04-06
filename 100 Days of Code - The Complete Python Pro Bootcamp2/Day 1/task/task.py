187706c1fdc553ee5d254afc5bcf6480d356b6db
EmilioF17 <163168512+EmilioF17@users.noreply.github.com>
2026-04-06 13:35:04 -0700

    Test1

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp2/Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("This is a band name generator. Follow the instructions below.")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of the pet you had when little?\n")
+print("Your band name is " + city_name + " " + pet_name)
